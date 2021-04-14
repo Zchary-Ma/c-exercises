1cd6647806373f23cadd7a0f88d4af6e7aa37125
Zchary-Ma <Zcharyma@gmail.com>
2021-04-14 23:08:47 +0800

    end(sort):bubble sort

--- src/examples/student-score-manager.c
+++ src/examples/student-score-manager.c
@@ -15,12 +15,9 @@ status ReadScore(int score[], int *pCount);
 
 status SortingScore(int score[], int n, int isAscending);
 
-
 int main() {
-    int nums[MAX_SIZE]; // 学号数组
     int scores[MAX_SIZE]; // 分数数组
     int count = 0;
-    int maxScore, maxScoreNum;
     printf("----Welcome------\n");
     ReadScore(scores, &count);
     printf("the original data items are:\n");
@@ -48,9 +45,9 @@ status PrintScore(int score[], int n) {
     status s = fail;
     if (score && n > 0) {
         s = success;
-    }
-    for (int i = 0; i < n; ++i) {
-        printf("%d ", score[i]);
+        for (int i = 0; i < n; ++i) {
+            printf("%d ", score[i]);
+        }
     }
     printf("\n");
     return s;
@@ -64,7 +61,7 @@ status ReadScore(int score[], int *pCount) {
         printf("Input Score:");
         i++;
     }
-    // NOTE put EOF to end while loop
+    // NOTE put "EOF" to end while loop
     *pCount = i;
     if (i > 10000) {
         s = fail;
@@ -75,20 +72,21 @@ status ReadScore(int score[], int *pCount) {
 
 status SortingScore(int score[], int n, int isAscending) {
     status s = fail;
+    int swapped = 0;
     // bubble sort
-    for (int i = 0; i < n; ++i) {
-        for (int j = i; j < n - i - 1; ++j) {
-            // ascending
-            if (isAscending > 0) {
-                if (score[j] < score[j + 1]) {
-                    Swap(&score[j], &score[j + 1]);
-                }
-            } else {
-                if (score[j] > score[j + 1]) {
-                    Swap(&score[j], &score[j + 1]);
-                }
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        printf("i:%d\t", i);
+        for (j = 0; j < n - i - 1; j++) {
+            printf("j:%d\t", j);
+            if (score[j] > score[j + 1]) {
+                Swap(&score[j], &score[j + 1]);
+                swapped = 1;
             }
-            // descending
+        }
+        printf("\n");
+        if (swapped == 0) {
+            return s;
         }
     }
     return s;
